708fd55d1702eb81c6956302cd1d3af017f6bbdd
Don Pflaster <dpflaster@gmail.com>
2021-02-08 09:05:25 -0500

    Build simple distillery release with Logflare backend

--- mix.exs
+++ mix.exs
@@ -2,12 +2,14 @@ defmodule DistilleryExample.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :distillery_example,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :distillery_example,
+      version: "0.1.0",
+      elixir: "~> 1.10",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
@@ -15,8 +17,7 @@ defmodule DistilleryExample.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger],
-     mod: {DistilleryExample.Application, []}]
+    [extra_applications: [:logger], mod: {DistilleryExample.Application, []}]
   end
 
   # Dependencies can be Hex packages:
@@ -29,6 +30,13 @@ defmodule DistilleryExample.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:distillery, "~> 1.3.5"}]
+    [
+      {:distillery, "~> 2.1",
+       warn_missing: false,
+       git: "git@github.com:bitwalker/distillery.git",
+       branch: "master",
+       override: true},
+      {:logflare_logger_backend, "0.8.0-rc.6"}
+    ]
   end
 end
--- rel/config.exs
+++ rel/config.exs
@@ -6,15 +6,14 @@ Path.join(["rel", "plugins", "*.exs"])
 |> Path.wildcard()
 |> Enum.map(&Code.eval_file(&1))
 
-use Mix.Releases.Config,
-    # This sets the default release built by `mix release`
-    default_release: :default,
-    # This sets the default environment used by `mix release`
-    default_environment: Mix.env()
+use Distillery.Releases.Config,
+  # This sets the default release built by `mix release`
+  default_release: :default,
+  # This sets the default environment used by `mix release`
+  default_environment: Mix.env()
 
 # For a full list of config options for both releases
 # and environments, visit https://hexdocs.pm/distillery/configuration.html
-
 
 # You may define one or more environments in this file,
 # an environment's settings will override those of a release
@@ -22,15 +21,15 @@ use Mix.Releases.Config,
 # and environment configuration is called a profile
 
 environment :dev do
-  set dev_mode: true
-  set include_erts: false
-  set cookie: :"wjWH_IYLTWPVOxaqV]77!bO~pt=N*z]m!xO!*T7{)q_tR.:O%/N4p51QE.ZxBkGN"
+  set(dev_mode: true)
+  set(include_erts: false)
+  set(cookie: :"wjWH_IYLTWPVOxaqV]77!bO~pt=N*z]m!xO!*T7{)q_tR.:O%/N4p51QE.ZxBkGN")
 end
 
 environment :prod do
-  set include_erts: true
-  set include_src: false
-  set cookie: :"^1%s>vy&Glln`2j;(GUo!pG7=y~hDcb9?UbUZuv)aN~VkMXp:)*))R`?8@MI!K`U"
+  set(include_erts: true)
+  set(include_src: false)
+  set(cookie: :"^1%s>vy&Glln`2j;(GUo!pG7=y~hDcb9?UbUZuv)aN~VkMXp:)*))R`?8@MI!K`U")
 end
 
 # You may define one or more releases in this file.
@@ -39,9 +38,11 @@ end
 # will be used by default
 
 release :distillery_example do
-  set version: current_version(:distillery_example)
-  set applications: [
-    :runtime_tools
-  ]
-end
+  set(version: current_version(:distillery_example))
 
+  set(
+    applications: [
+      :runtime_tools
+    ]
+  )
+end
